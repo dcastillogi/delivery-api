629ae2522fa49eefcd18ecd34ec28738880fbb55
Daniel Castillo <dev@dcastillogi.com>
2024-06-22 11:04:54 -0500

    data upload

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+
+const RECOMMENDED_PRICE = {
+    'N': 14500,
+    'DA': 22500,
+    'L': 7000
+} // Based on "Interrapidisimo" prices
+
+const getData = async (): Promise<any> => {
+
+}
+
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+        const { searchParams } = new URL(request.url);
+        const origin = searchParams.get('origin');
+        const destination = searchParams.get('destination');
+        
+        if (!origin || !destination) {
+            return new Response('Missing fields', { status: 400 });
+        }
+
+
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
